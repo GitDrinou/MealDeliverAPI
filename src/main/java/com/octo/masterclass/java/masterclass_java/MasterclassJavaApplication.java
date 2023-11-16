ac07a9f60bc05d27567d9e86e8b5c46224902ba4
sandrine.brival <sandrine.brival@octo.com>
2023-11-16 13:47:37 +0100

    start

---
+++ src/main/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplication.java
@@ -0,0 +1,13 @@
+package com.octo.masterclass.java.masterclass_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterclassJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterclassJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octo.masterclass.java.masterclass_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterclassJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
