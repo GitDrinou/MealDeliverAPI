792f812ceb43cd44759e9a9f257f5c5936e92b2f
sandrine.brival <sandrine.brival@octo.com>
2023-11-17 15:25:28 +0100

    formuleController display formule et calcule prix formule

--- src/main/java/com/octo/masterclass/java/masterclass_java/api/FormuleController.java
+++ src/main/java/com/octo/masterclass/java/masterclass_java/api/FormuleController.java
@@ -1,26 +1,43 @@
 package com.octo.masterclass.java.masterclass_java.api;
 
 import com.octo.masterclass.java.masterclass_java.restaurant.Formule;
+import com.octo.masterclass.java.masterclass_java.restaurant.Ingredient;
 import com.octo.masterclass.java.masterclass_java.restaurant.Plat;
 import com.octo.masterclass.java.masterclass_java.restaurant.RestaurantService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController()
-@RequestMapping("/formule")
+@RequestMapping("/Formule")
 public class FormuleController {
 
-    //private final RestaurantService service;
+    final RestaurantService service;
+
     Formule mezzogiorno = new Formule("Mezzogiorno", List.of(
-            new Plat("Bruschetta Di Pomodora con Parma", 6.90, List.of()),
+            new Plat("Bruschetta Di Pomodora", 6.90, List.of(
+                    new Ingredient("Tartare de tomate", false, 0),
+                    new Ingredient("Echalotes", false, 1),
+                    new Ingredient("Parmigiano Reggiano", false, 0)
+            )),
             new Plat("Risotto al Sapore di Mare",14.80, List.of())
     ));
     Formule pizza = new Formule("Pizza", List.of());
 
     List<Formule> carte = List.of(mezzogiorno, pizza);
 
-    public FormuleController(RestaurantService service){ this.service = service; }
+    public FormuleController(RestaurantService service){this.service = service; }
 
+    @GetMapping
+    List<Formule> getFormules(){
+        return carte;
+    }
+
+    @GetMapping("/prix")
+    Double getPrixMezzo() {
+        return service.calculerPrixFormule(mezzogiorno);
+    }
 }
+
--- src/main/java/com/octo/masterclass/java/masterclass_java/restaurant/Ingredient.java
+++ src/main/java/com/octo/masterclass/java/masterclass_java/restaurant/Ingredient.java
@@ -4,4 +4,34 @@ public class Ingredient {
     String nom;
     Boolean allergene;
     Integer spicy;
+
+    public Ingredient(String nom, Boolean allergene, Integer spicy) {
+        this.nom = nom;
+        this.allergene = allergene;
+        this.spicy = spicy;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public Boolean getAllergene() {
+        return allergene;
+    }
+
+    public Integer getSpicy() {
+        return spicy;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAllergene(Boolean allergene) {
+        this.allergene = allergene;
+    }
+
+    public void setSpicy(Integer spicy) {
+        this.spicy = spicy;
+    }
 }
