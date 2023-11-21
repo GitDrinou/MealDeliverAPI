2a73680f9922b92749895a21de3651e70ce7a12d
sandrine.brival <sandrine.brival@octo.com>
2023-11-21 18:00:17 +0100

    formule persistent / repo

--- src/main/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplication.java
+++ src/main/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplication.java
@@ -1,8 +1,10 @@
 package com.octo.masterclass.java.masterclass_java;
 
+import com.octo.masterclass.java.masterclass_java.rerpository.FormuleRepository;
 import com.octo.masterclass.java.masterclass_java.rerpository.IngredientRepository;
 import com.octo.masterclass.java.masterclass_java.rerpository.PlatRepository;
 import com.octo.masterclass.java.masterclass_java.persistence.Plat;
+import com.octo.masterclass.java.masterclass_java.persistence.Formule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -22,16 +24,38 @@ public class MasterclassJavaApplication implements CommandLineRunner {
 	PlatRepository platRepository;
 	@Autowired
 	IngredientRepository ingredientRepository;
+	@Autowired
+	FormuleRepository formuleRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+
 		Ingredient tomate = new Ingredient("tomate", false, 0);
 		Ingredient echalote = new Ingredient("Echalotes", false, 1);
 		Ingredient parmesan = new Ingredient("Parmigiano Reggiano", false, 0);
+		Ingredient riz = new Ingredient("Riz", false, 0);
+		Ingredient champignon = new Ingredient("Champignon", true, 0);
+		Ingredient farine = new Ingredient("Farine", true, 0);
+		Ingredient mozzarella = new Ingredient("Mozzarella", true, 0);
+		Ingredient sauceTomate = new Ingredient("Sauce tomate", false, 0);
+		Ingredient basilic = new Ingredient("Basilic", false, 0);
+		Ingredient huileOlive = new Ingredient("Huile d'olive", false, 0);
+		Ingredient banane = new Ingredient("Banane", false, 0);
+		Ingredient orange = new Ingredient("Orange", false, 0);
+		Ingredient pomme = new Ingredient("Pomme", false, 0);
+		Ingredient framboise = new Ingredient("Framboise", false, 0);
 
-		Plat bruschetta = new Plat("bruschetta", 5.90, 1, List.of(tomate,echalote,parmesan));
+		Plat bruschetta = new Plat("bruschetta", 5.90, 5, List.of(tomate,echalote,parmesan));
+		Plat risotto = new Plat("Risotto al Funghi",14.80, 45, List.of(riz, champignon));
+		Plat pizzaMarguerita = new Plat("Margherita", 8.00, 10, List.of(farine, sauceTomate, mozzarella, basilic, huileOlive));
+		Plat insalataFrutti = new Plat("Insalata di frutti", 4.60, 5, List.of(banane, orange, pomme, framboise));
+
+		Formule midi = new Formule("Midi", List.of(bruschetta, risotto));
+		Formule pizza = new Formule("Pizza", List.of(pizzaMarguerita, insalataFrutti));
+
 
 		ingredientRepository.saveAll(List.of(tomate,echalote,parmesan));
 		platRepository.save(bruschetta);
+		formuleRepository.saveAll(List.of(midi, pizza));
 	}
 }
--- src/main/java/com/octo/masterclass/java/masterclass_java/api/FormuleController.java
+++ src/main/java/com/octo/masterclass/java/masterclass_java/api/FormuleController.java
@@ -1,5 +1,7 @@
 package com.octo.masterclass.java.masterclass_java.api;
 
+import com.octo.masterclass.java.masterclass_java.persistence.Formule;
+import com.octo.masterclass.java.masterclass_java.rerpository.FormuleRepository;
 import com.octo.masterclass.java.masterclass_java.restaurant.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,35 +13,27 @@ import java.util.List;
 @RequestMapping("/formules")
 public class FormuleController {
 
-    final RestaurantService service;
+    FormuleRepository formuleRepository;
 
-    Formule midi = new Formule("Midi", List.of(
-            new Entree("Bruschetta Di Pomodora", 6.90, List.of(
-                    new Ingredient("Tartare de tomate", false, 0),
-                    new Ingredient("Echalotes", false, 1),
-                    new Ingredient("Parmigiano Reggiano", false, 0)
-            ), true),
-            new Plat("Risotto al Sapore di Mare",14.80, List.of()),
-            new Dessert("Tiramisu", 4.90, List.of())
-    ));
+    public FormuleController(FormuleRepository formuleRepository) {
+        this.formuleRepository = formuleRepository;
+    }
 
-    Formule pizza = new Formule("Pizza", List.of(
-            new Plat("Margherita", 8.00, List.of()),
-            new Plat("Insalata di frutti", 4.60, List.of())
-    ));
+    public Iterable<Formule> listeDesFormules() {
+        return formuleRepository.findAll();
+    }
 
-    List<Formule> formules = List.of(midi, pizza);
 
-    public FormuleController(RestaurantService service){this.service = service; }
-
-    @GetMapping
-    List<Formule> getFormules(){
+    //final RestaurantService service;
+    //public FormuleController(RestaurantService service){this.service = service; }
+    //@GetMapping
+    /* List<Formule> getFormules(){
         return formules;
-    }
+    }*/
 
-    @GetMapping("/prix")
-    Double getPrixFormuleMidi() {
+    //@GetMapping("/prix")
+    /* Double getPrixFormuleMidi() {
         return service.calculerPrixFormule(midi);
-    }
+    }*/
 }
 
