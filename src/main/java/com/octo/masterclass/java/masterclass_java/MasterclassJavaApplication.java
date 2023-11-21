4ae59641ef0feb2a04680d8253e81269bf3756d7
sandrine.brival <sandrine.brival@octo.com>
2023-11-21 15:34:49 +0100

    Debug liste ingredient / plat

--- src/main/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplication.java
+++ src/main/java/com/octo/masterclass/java/masterclass_java/MasterclassJavaApplication.java
@@ -1,5 +1,6 @@
 package com.octo.masterclass.java.masterclass_java;
 
+import com.octo.masterclass.java.masterclass_java.rerpository.IngredientRepository;
 import com.octo.masterclass.java.masterclass_java.rerpository.PlatRepository;
 import com.octo.masterclass.java.masterclass_java.persistence.Plat;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,14 +20,18 @@ public class MasterclassJavaApplication implements CommandLineRunner {
 
 	@Autowired
 	PlatRepository platRepository;
+	@Autowired
+	IngredientRepository ingredientRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		Ingredient tomate = new Ingredient("tomate", false, 0);
 		Ingredient echalote = new Ingredient("Echalotes", false, 1);
 		Ingredient parmesan = new Ingredient("Parmigiano Reggiano", false, 0);
+
 		Plat bruschetta = new Plat("bruschetta", 5.90, 1, List.of(tomate,echalote,parmesan));
 
+		ingredientRepository.saveAll(List.of(tomate,echalote,parmesan));
 		platRepository.save(bruschetta);
 	}
 }
